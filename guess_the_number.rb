0ad32a9b939a507463eec19ef8245a415dc80fb0
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-04 17:16:56 +0200

    Divider method & Welcome

---
+++ guess_the_number.rb
@@ -0,0 +1,15 @@
+def print_divider
+  puts "*" * 40
+  puts "\n"
+end
+
+print_divider
+/n
+/n
+  puts "...WELCOME TO THE NUMBER GUESSING GAME..."
+/n
+/n
+print_divider
+
+# Generate random secret number between 1 and 100
+secret_num = 1 + rand(100)
