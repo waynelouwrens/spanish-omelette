9b40b4b24fc42f52c542e12f992b13575411fe7f
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-04 16:06:57 +0200

    Ask the user if they have all the ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -12,11 +12,11 @@ NUM_POTATOES = 5
 NUM_EGGS = 6
 
 ingredients = [
-  {name: "potatoes",quantity: NUM_POTATOES}
-  {name: "eggs",quantity: NUM_EGGS}
-  {name: "onion",quantity: 1}
-  {name: "oil",quantity: "some"}
-  {name: "salt",quantity: "some"}
+  {name: "potatoes",quantity: NUM_POTATOES},
+  {name: "eggs",quantity: NUM_EGGS},
+  {name: "onion",quantity: 1},
+  {name: "oil",quantity: "some"},
+  {name: "salt",quantity: "some"},
 ]
 
 #Printing Out the Ingredients
@@ -28,4 +28,24 @@ puts "You will the following ingredients:"
 
 ingredients.each do |ingredient|
   puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
+end
+
+# Asking user if he got all ingredients
+
+loop do
+  puts "\n"
+  print_divider
+
+  puts "Got all ingredients you need? (Y\N)"
+  answer = gets.chomp.upcase
+
+  if answer == "N"
+    puts "You need to go shopping"
+    print_progress_bar
+  elsif answer == "Y"
+    puts "Let's start cooking!"
+    break
+  else
+    puts "That's not a valid input. Try again"
+  end
 end
